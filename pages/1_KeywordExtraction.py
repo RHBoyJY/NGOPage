f5bfa695be6ccde1be698c39ce45d247344f7a9b
RHBoyJY <46002103+RHBoyJY@users.noreply.github.com>
2023-11-21 00:39:01 +0800

    Update 1_KeywordExtraction.py

--- pages/1_KeywordExtraction.py
+++ pages/1_KeywordExtraction.py
@@ -147,7 +147,7 @@ if st.checkbox('找出要點'):
         bytes_data = uploaded_file.getvalue()
         result_byte_data1, result_byte_data2= process_excel_data(bytes_data)
         st.download_button(
-            label="Download All Result as Excel",
+            label="Download Full Result as Excel",
             data=result_byte_data1,
             file_name='CombinedExcel.xlsx',
             mime='Excel/xlsx',
